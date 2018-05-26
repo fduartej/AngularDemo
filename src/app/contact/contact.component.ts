fbbe88dd9c4e5bd5d3363df61ef24ea77998fa87
Fred Duarte <fduartej@gmail.com>
2018-05-26 09:57:48 -0500

    Agregando Cambios al Form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+   pageTitle: string = 'Contact';
+
   constructor() { }
 
   ngOnInit() {
